0840048ed409edc0d20cbc7bad4ff34a308e3607
Tohjuler <tobias@tohjuler.dk>
2026-01-12 16:52:21 +0100

    init

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,25 @@
+import { db } from "@server-updator/db";
+import * as schema from "@server-updator/db/schema/auth";
+import { env } from "@server-updator/env/server";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "sqlite",
+
+    schema: schema,
+  }),
+  trustedOrigins: [env.CORS_ORIGIN],
+  emailAndPassword: {
+    enabled: true,
+  },
+  advanced: {
+    defaultCookieAttributes: {
+      sameSite: "none",
+      secure: true,
+      httpOnly: true,
+    },
+  },
+  plugins: [],
+});
---
+++ packages/db/src/index.ts
@@ -0,0 +1,8 @@
+import { env } from "@server-updator/env/server";
+import { drizzle } from "drizzle-orm/bun-sqlite";
+
+import * as schema from "./schema";
+
+export const db = drizzle({ connection: {
+  source: env.DB_FILE_NAME || "",
+}, schema });
