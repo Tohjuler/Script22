23c9af060bafbd453dac0c2a19a7c90a548540cf
Tohjuler <tobias@tohjuler.dk>
2026-03-11 01:09:18 +0100

    feat: Add cleanup cron job to manage job timeouts and state

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -10,7 +10,7 @@ import { appRouter } from "@script22/api/routers/index";
 import { auth } from "@script22/auth";
 import { migrateDb } from "@script22/db";
 import { env } from "@script22/env/server";
-import { handleCronJobs } from "@script22/logic/cronJobs";
+import { handleCronJobs, startCleanupCronJob } from "@script22/logic/cronJobs";
 import { logger } from "@script22/logic/logger";
 import { getSetting, setSetting } from "@script22/logic/settings";
 import { Elysia } from "elysia";
@@ -102,5 +102,9 @@ const app = new Elysia()
 		logger.info("Starting cron jobs...");
 		await handleCronJobs().catch((err) => {
 			logger.error(err, "Error starting cron jobs:");
+		});
+
+		startCleanupCronJob().catch((err) => {
+			logger.error(err, "Error starting cleanup cron job:");
 		});
 	});
--- packages/logic/src/cronJobs.ts
+++ packages/logic/src/cronJobs.ts
@@ -1,7 +1,10 @@
 import { db } from "@script22/db";
+import { Tables } from "@script22/db/schema/main";
+import { env } from "@script22/env/server";
 import { YAML } from "bun";
 import { CronJob } from "cron";
-import { queueJob } from "./jobRunner";
+import { eq } from "drizzle-orm";
+import { checkJobTimouts, queueJob } from "./jobRunner";
 import { logger } from "./logger";
 import { type JobConfig, jobConfig } from "./types";
 
@@ -27,6 +30,51 @@ export async function handleCronJobs() {
 		"Cron jobs have been set up, total: %d",
 		Object.keys(cronJobs).length,
 	);
+}
+
+export async function startCleanupCronJob() {
+	new CronJob(
+		"* * * * *", // Every minute
+		async () => {
+			logger.debug("Running cleanup cron job");
+			checkJobTimouts().catch((err) => {
+				logger.error(err, "Error checking job timeouts:");
+			});
+
+			// Cleanup jobRuns with state running in db
+			// ---
+			const maxTime = env.JOB_RUNNER_JOB_TIMEOUT * 60 * 1000;
+
+			const jobs = await db.query.jobRun.findMany({
+				columns: { id: true, createdAt: true },
+				where: (jobRun, { eq }) => eq(jobRun.state, "running"),
+			});
+			const now = new Date();
+			for (const job of jobs) {
+				if (now.getTime() - new Date(job.createdAt).getTime() > maxTime) {
+					await db
+						.update(Tables.jobRun)
+						.set({
+							state: "failed",
+							finishedAt: new Date(),
+							output: JSON.stringify([
+								{
+									status: -1,
+									stdout: "",
+									stderr: "Job cancelled: Timeout exceeded",
+								},
+							]),
+						})
+						.where(eq(Tables.jobRun.id, job.id));
+					logger.info("Marked job ID %d as failed", job.id);
+				}
+			}
+		},
+		null,
+		true,
+		process.env.TZ || undefined,
+	);
+	logger.info("Cleanup cron job has been set up to run every minute");
 }
 
 export async function handleConfigUpdate(jobId: number, config: JobConfig) {
